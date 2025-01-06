3b1c0776387a74a11fdb36b36d79ce1e36ce79ca
awk <abkhlaf1@gmail.com>
2025-01-07 00:10:13 +0200

    rr

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -3,28 +3,14 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const {
-  DB_NAME,
-  DB_USER,
-  DB_PASSWORD,
-  DB_HOST,
-  DB_PORT
-} = process.env;
-
-const sequelize = new Sequelize({
-  database: DB_NAME,
-  username: DB_USER,
-  password: DB_PASSWORD,
-  host: DB_HOST,
-  port: DB_PORT,
+const sequelize = new Sequelize(process.env.DB_URL, {
   dialect: 'postgres',
   dialectOptions: {
     ssl: {
       require: true,
       rejectUnauthorized: false
     }
-  },
-  logging: false
+  }
 });
 
 export default sequelize;
